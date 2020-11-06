88dde2bee2d7d7f265116808dbd77e340040c0c5
sedx876 <swatkins876@gmail.com>
2020-11-06 06:24:32 -0500

    code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,29 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/App.css';
+import { AuthProvider } from './context/auth';
+import AuthRoute from './util/AuthRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import MenuBar from './components/MenuBar';
+import SinglePost from './pages/SinglePost';
 
 
 
 function App() {
   return (
-    <Router>
-      <Container>
-        <MenuBar/>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/login' component={Login}/>
-      <Router exact path='/register' component={Register}/>
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
